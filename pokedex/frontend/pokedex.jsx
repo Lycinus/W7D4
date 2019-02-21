2dd359b679e7efdbe615c6b6ffb9acc9d4f78a3f
Ryan Alejandro <alejandr257@gmail.com>
2019-02-20 18:10:02 -0800

    Finish thunks in phase 2 of pokedex project

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root')
+    const store = configureStore()
+
+    window.store = store
+    
+    // window.getState = store.getState; 
+    // window.dispatch = store.dispatch;
+
+    window.fetchAllPokemon = fetchAllPokemon
+    window.receiveAllPokemon = receiveAllPokemon
+    window.requestAllPokemon = requestAllPokemon
 
     ReactDOM.render(
         <h1>d3!</h1>,
