cf55feb7c03f6c39667e3c8c1005f97c453b3b3e
Ryan Alejandro <alejandr257@gmail.com>
2019-02-21 11:26:01 -0800

    Finish phase 3

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { fetchAllPokemon } from './util/api_util';
 import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+import { selectAllPokemon } from './reducers/selectors';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root')
     const store = configureStore()
-
-    window.store = store
     
-    // window.getState = store.getState; 
-    // window.dispatch = store.dispatch;
-
-    window.fetchAllPokemon = fetchAllPokemon
-    window.receiveAllPokemon = receiveAllPokemon
+    window.getState = store.getState; 
+    window.dispatch = store.dispatch;
     window.requestAllPokemon = requestAllPokemon
+    window.selectAllPokemon = selectAllPokemon
 
     ReactDOM.render(
         <h1>d3!</h1>,
